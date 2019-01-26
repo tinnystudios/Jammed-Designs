28e043c973e82bdee16e54639cb931feb31dac75
MichaelVatskalis <prometheus146@gmail.com>
2019-01-26 17:25:35 +1100

    Made an overlay prefab

---
+++ Jammed Designs/Assets/_Working Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        GameManager.Instance.CurrentState = GameManager.SystemState.Running;
+        Destroy(gameObject);
+    }
+
+}
---
+++ Jammed Designs/Assets/_Working Assets/Scripts/UI/MenuSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSpawner : MonoBehaviour {
+    public GameObject MenuToSpawn;
+	public void SpawnMenu()
+    {
+        var go = Instantiate(MenuToSpawn);
+        go.transform.SetParent(transform.parent);
+        go.GetComponent<RectTransform>().transform.position = Vector3.zero;
+        Time.timeScale = 0;
+        GameManager.Instance.CurrentState = GameManager.SystemState.Paused;
+    }
+}
