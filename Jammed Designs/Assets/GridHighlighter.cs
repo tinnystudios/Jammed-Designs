88bcc7ea31e4dcdcd4b7d1ac79edd7a0072376dc
tinny <tinnystudios@gmail.com>
2019-01-26 12:49:18 +1100

    # Conflicts: #	Jammed Designs/Assets/_Working Assets/Models/House-WorkingFiles.mb

---
+++ Jammed Designs/Assets/GridHighlighter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridHighlighter : MonoBehaviour
+{
+    public bool IsValid = true;
+
+    private void OnTriggerStay(Collider col)
+    {
+        var gridNode = col.GetComponentInParent<GridNode>();
+
+        if (gridNode != null)
+        {
+            if (!gridNode.Usable)
+            {
+                IsValid = false;
+            }
+
+            if (IsValid)
+                gridNode.Highlight();
+            else
+                gridNode.Invalid();
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        var gridNode = col.GetComponentInParent<GridNode>();
+        if (gridNode != null)
+        {
+            gridNode.UnHighlight();
+        }
+    }
+}
